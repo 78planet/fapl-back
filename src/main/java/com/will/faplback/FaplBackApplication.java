1c08a5867d1a67c40e91ed9a19aaaa4fa79531c0
will kim <will@willui-MacBookPro.local>
2024-01-23 22:54:42 +0900

    project initial setup

---
+++ src/main/java/com/will/faplback/FaplBackApplication.java
@@ -0,0 +1,13 @@
+package com.will.faplback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaplBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaplBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/faplback/FaplBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.faplback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaplBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
